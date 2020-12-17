3f23cb04569ea2aac93f2a192ca0d0607de63358
Valerii Logvin <valerii.logvin@gmail.com>
2020-12-17 13:25:34 +0200

    [subscribe] card scripts

--- src/2020-12-16--subscription/js/main.js
+++ src/2020-12-16--subscription/js/main.js
@@ -3,6 +3,15 @@ let $freeSubscribe = document.querySelector('.js_free_subscribe_block');
 let $overlay = document.querySelector('.js_overlay');
 let myElement = document.querySelector('.js_swipe_bottom');
 
+let $freeInputCard = $freeSubscribe.querySelector('.js_input_card');
+let $freeInputCardIcon = $freeSubscribe.querySelector('.js_card_icon');
+let $freeInputCardDate = $freeSubscribe.querySelector('.js_input_card_date');
+let $freeInputCardCvv = $freeSubscribe.querySelector('.js_input_card_cvv');
+let $freeInputBtnNext = $freeSubscribe.querySelector('.js_card_next_step');
+let $freeInputInfo = $freeSubscribe.querySelector('.js_card_info');
+let $btnAddCard = $freeSubscribe.querySelector('.js_btn_add_card');
+
+
 myElement.addEventListener("touchstart", startTouch, false);
 myElement.addEventListener("touchmove", moveTouch, false);
 
@@ -64,3 +73,118 @@ $overlay.addEventListener('click', ()=> {
 function openFreeSubscribe(){
     $freeSubscribe.classList.add('active')
 }
+
+$freeInputCard.oninput = function () {
+    validatorCard($freeInputCard);
+};
+
+
+
+function validatorCard($input) {
+    let cardNumber;
+    let tempVal = $input.value.replace(/\D/g, '');
+
+    cardNumber = (tempVal.slice(0,4).replace(/(.{4})/g, '$1 ') +
+        tempVal.slice(4,8).replace(/(.{4})/g, '$1 ') +
+        tempVal.slice(8,12).replace(/(.{4})/g, '$1 ') +
+        tempVal.slice(12,16)).trim();
+
+    $input.value = $input.value = cardNumber;
+
+    let firstNum = +tempVal.slice(0, 1);
+
+    if(firstNum === 2){
+        $freeInputCardIcon.setAttribute('src', 'img/mir.svg')
+    }else if(firstNum === 5){
+        $freeInputCardIcon.setAttribute('src', 'img/mc.svg')
+    }else if(firstNum === 4){
+        $freeInputCardIcon.setAttribute('src', 'img/visa.svg')
+    }else {
+        $freeInputCardIcon.setAttribute('src', 'img/card.svg')
+
+    }
+
+    if($input.value.length === 19){
+        $freeInputBtnNext.style = 'opacity: 1; visibility: visible';
+        let lastNumbers = $input.value.slice(15,19);
+        $freeInputInfo.innerText = "***" + lastNumbers;
+        console.log(lastNumbers);
+    }
+}
+
+function inputDate(){
+    $freeInputBtnNext.style = 'opacity: 0; visibility: hidden';
+    $freeInputCard.style.display = 'none';
+    $freeInputInfo.style.display = 'block';
+    $freeInputCardDate.style = 'opacity: 1; visibility: visible';
+    $freeInputCardCvv.style = 'opacity: 1; visibility: visible';
+    $freeInputCardDate.focus();
+}
+
+$freeInputCardDate.oninput = function () {
+    validatorCardDate($freeInputCardDate);
+};
+
+function validatorCardDate($input) {
+    let cardNumber;
+    let tempVal = $input.value.replace(/\D/g, '');
+
+    cardNumber = (tempVal.slice(0,2).replace(/(.{2})/g, '$1/') +
+        tempVal.slice(2,4)).trim();
+
+    $input.value = $input.value = cardNumber;
+
+    if($input.value.length === 5){
+        $freeInputCardCvv.focus();
+    }
+}
+
+$freeInputCardCvv.oninput = function () {
+    let dateLength = $freeInputCardDate.value.length,
+        cvvLength = $freeInputCardCvv.value.length;
+    if(dateLength === 5 && cvvLength === 3){
+        $btnAddCard.classList.remove('disabled')
+    }
+};
+
+function addFreeCard() {
+    let card = $freeInputCard.value,
+        date = $freeInputCardDate.value,
+        cvv = $freeInputCardCvv.value;
+
+    alert("card:" + card + " date:" + date + " cvv:" + cvv);
+}
+
+// function validatorName() {
+//     let value = $inputFirstName.value;
+//
+//     let rus = value.match(/[А-Яа-яёЁЇїІіЄєҐґ]/g),
+//         iChars = value.match(/[^\d\sA-Z]/gi),
+//         numbers = value.match(/[0-9]/g),
+//         space = value.match(/\s/);
+//
+//     if (value.length === 0) {
+//         validFirstName = false;
+//     }
+//
+//     for (let i = 0; i < value.length; i++) {
+//
+//         $inputFirstName.value = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+//
+//         if (iChars || rus || space) {
+//             showError('Имя может содержать только латинские символы.');
+//             validFirstName = false;
+//         } else if (numbers) {
+//             showError('Имя не может содержать цифры.');
+//             validFirstName = false;
+//         } else {
+//             validFirstName = true;
+//         }
+//     }
+//     if (!validFirstName) {
+//         $inputFirstName.classList.add('error');
+//     } else {
+//         showError('');
+//         $inputFirstName.classList.remove('error');
+//     }
+// }
